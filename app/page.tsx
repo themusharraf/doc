cf3fc570ceac1e97fd58603fecf3466fb1c6910d
Musharraf Ibragimov <122869450+themusharraf@users.noreply.github.com>
2024-12-28 20:04:25 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex sm:min-h-[85.5vh] min-h-[85vh] flex-col items-center justify-center text-center px-2 sm:py-8 py-12">
       <Link
-        href="https://github.com/nisabmohd/Aria-Docs"
+        href="https://github.com/themusharraf"
         target="_blank"
         className="mb-5 sm:text-lg flex items-center gap-2 underline underline-offset-4 sm:-mt-12"
       >
@@ -15,13 +15,13 @@ export default function Home() {
         <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
       </Link>
       <h1 className="text-3xl font-bold mb-4 sm:text-6xl">
-        Effortlessly build stunning documentation sites with Next.js and server
-        components.
+        Shunchaki bugun dan boshlang va AllNc sizni muvaffaqiyat sari olib boradi.
+        Orqaga qadam tashlamang, o'z imkoniyatlaringizni sinab ko'ring va dunyoni
+        o'zgartiradigan loyihangizni bugundan boshlang.
       </h1>
       <p className="mb-8 sm:text-lg max-w-[800px] text-muted-foreground">
-        This feature-packed documentation template, built with Next.js, offers a
-        sleek and responsive design, perfect for all your project documentation
-        needs.
+        Rivojlanish yo‘lida biz bilan birga bo‘ling, g‘oyalaringizni kuchli hujjatlar
+        bilan hayotga tatbiq qiling va dunyo bilan baham ko‘ring.
       </p>
       <div className="flex flex-row items-center gap-5">
         <Link
