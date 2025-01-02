f6c59e52a6893f1f286fd5fee6c377369ce3a890
mrx <meibrohimov@gmail.com>
2025-01-02 21:40:31 +0500

    add new python interview part 1

--- components/footer.tsx
+++ components/footer.tsx
@@ -7,12 +7,12 @@ export function Footer() {
     <footer className="border-t w-full h-16">
       <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
         <div className="flex items-center gap-3">
-          <CommandIcon className="sm:block hidden w-5 h-5 text-muted-foreground" />
+          {/*<CommandIcon className="sm:block hidden w-5 h-5 text-muted-foreground" />*/}
           <p className="text-center">
-            Manba kodi GitHub’da mavjud.{" "}
+            GPL-3.0 Licensed | Hamma huquqlar himoyalangan 2025 ©Uzbek Developers.{" "}
             <Link
               className="px-1 underline underline-offset-2"
-              href="https://github.com/themusharraf"
+              href="https://github.com/themusharraf/doc/tree/main?tab=GPL-3.0-1-ov-file"
             >
               GitHub
             </Link>
